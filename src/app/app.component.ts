e88351cb643339921fa70dfafa69293deb09f770
milindturerao <tureraomilind8055@gmail.com>
2022-09-07 23:31:15 -0700

    project ready to code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'oems';
+
+  sweetalert(){
+    Swal.fire("Sweet allert add");
+  }
+
+
 }
